d6cec72a408fcac0d2618ecfdc3eb263478cd4fe
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-16 12:51:03 +0100

    Add files via upload

---
+++ Prva_Zadnja.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Unesi neku recenicu:");
+        string recenica = Console.ReadLine();
+
+        string[] rijeci = recenica.Split(' ');
+
+        string prvaRijec = rijeci[0];
+        string zadnjaRijec = rijeci[rijeci.Length - 1];
+
+        Console.WriteLine("Prva rijec: " + prvaRijec);
+        Console.WriteLine("Zadnja rijec: " + zadnjaRijec);
+
+        Console.ReadKey();
+    }
+}
+
